ac39cdffe327f7f957556a446ce0ca07f81f1baa
Dennis Kyalo <kyalodennis1738@gmail.com>
2025-03-30 10:50:04 +0300

    final

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 // Write your code in this file!
 // Define the currentUser variable
-const currentUser = "Grace Hopper";  // Change this to any name you'd like
+const currentUser = "Grace Hopper";
+ // Change this to any name you'd like
 
 // Define the welcomeMessage variable
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+const welcomeMessage = 'Welcome to Flatbook, Grace Hopper!';
+
 
 // Define the excitedWelcomeMessage variable (uppercase version)
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, GRACE HOPPER!';
+
 
 // Define the shortGreeting variable (only the first letter of the user's name)
-const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+const shortGreeting = 'Welcome, G!';
